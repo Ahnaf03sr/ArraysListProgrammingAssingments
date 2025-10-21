951c35031d7107c2768a1b560abe989c2f09fc54
Ahnaf03sr <amk1010114@student.hamk.fi>
2025-10-21 17:10:12 +0300

    Added a new color Red

--- App.java
+++ App.java
@@ -1,10 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
         String[] color;
-        color = new String[3];
+        color = new String[4];
         color[0]="Green";
         color[1]="Blue";
         color[2]="Yellow";
+        // Add another color "red"
+        color[3]="Red";
+         // Add another color "Black"
+         //color[4]= "Black";
         // System.out.println(color[1]);
          for
          ( int i = 0; i< color.length; i++)
