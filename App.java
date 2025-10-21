c1d5de69cf0a072bca2ed1cf87bf37760b071e5e
Ahnaf03sr <amk1010114@student.hamk.fi>
2025-10-21 16:42:03 +0300

    App.java: first comment in arrays list program where can see the color list

---
+++ App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String[] color;
+        color = new String[3];
+        color[0]="Green";
+        color[1]="Blue";
+        color[2]="Yellow";
+        // System.out.println(color[1]);
+         for
+         ( int i = 0; i< color.length; i++)
+         {
+            System.out.println(color[i]);
+         }
+        
+    }
+}
